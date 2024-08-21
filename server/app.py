7d8b190630ea01078b95a1a6aecad853e6d6c78e
Hottensiah Wanjiku <w.hottensiah96@gmail.com>
2024-08-21 21:59:31 +0300

    First commit

--- server/app.py
+++ server/app.py
@@ -4,5 +4,43 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<string:text>')
+def print_string(text):
+    print(text)
+    return text
+
+@app.route('/count/<int:number>')
+def count(number):
+    count = f''
+    for n in range(number):
+        count += f'{n}\n'
+    return count
+
+@app.route('/math/<int:num1>/<string:operation>/<int:num2>')
+def math(num1, num2, operation):
+    if operation == '+':
+        result = num1 + num2
+    elif operation == '-':
+        result = num1 - num2
+    elif operation == '*':
+        result = num1 * num2
+    elif operation == 'div':
+        if num2 != 0:
+            result = num1 / num2
+        else:
+            return 'Error: Division by zero'
+    elif operation == '%':
+        if num2 != 0:
+            result = num1 % num2
+        else:
+            return 'Error: Division by zero'
+    else:
+        return 'Error: Invalid operation'
+    return str(result)
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
